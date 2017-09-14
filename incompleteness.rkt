c65b9f89a4a609e31465aaa2ddedbe9fd73625bf
Takahiro Kobayashi <koba925@gmail.com>
2017-09-15 06:25:08 +0900

    Created a function ∃≦ and a macro ∃.

--- incompleteness.rkt
+++ incompleteness.rkt
@@ -3,6 +3,8 @@
 (require rackunit)
 (require (for-syntax syntax/parse))
 
+; 装備を整える
+
 (define (∀≦ max f)
   (let loop ((x 0))
     (cond ((> x max) #t)
@@ -24,3 +26,24 @@
 
 (check-true (∀ x ≦ 3 (< x 4)))
 (check-false (∀ x ≦ 3 (< x 3)))
+
+(define (∃≦ max f)
+  (let loop ((x 0))
+    (cond ((> x max) #f)
+          ((f x) #t)
+          (else (loop (+ x 1))))))
+
+(check-true (∃≦ 3 (λ (x) (= x 2))))
+(check-false (∃≦ 3 (λ (x) (= x 4))))
+
+(define-syntax (∃ stx)
+  (syntax-parse stx
+    #:literals (≦)
+    [(_ v:id ≦ max:expr body:expr)
+     #'(∃≦ max (λ (v) body))]))
+
+(check-true (∃ x ≦ 3 (= x 2)))
+(check-false (∃ x ≦ 3 (= x 4)))
+
+
+
