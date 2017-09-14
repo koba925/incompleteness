5a63cd619512bf77d729a1c198736a2b18c09cff
Takahiro Kobayashi <koba925@gmail.com>
2017-09-15 08:08:51 +0900

    Created ∃≦, ∃, Min≦ and Min.

--- Incompleteness 02.md
+++ Incompleteness 02.md
@@ -2,4 +2,37 @@
 
 ## 「10.8.1 装備を整える」の続き
 
-まずは`∀x≦M[...]`の定義から
+同様に`∃x≦M[...]`と`min x≦M[...]`を定義します
+
+```
+(define (∃≦ max f)
+  (let loop ((x 1))
+    (cond ((> x max) #f)
+          ((f x) #t)
+          (else (loop (+ x 1))))))
+
+(define-syntax (∃ stx)
+  (syntax-parse stx
+    #:literals (≦)
+    [(_ v:id ≦ max:expr body:expr)
+     #'(∃≦ max (λ (v) body))]))
+     
+(define (Min≦ max f)
+  (let loop ((x 1))
+    (cond ((> x max) 0)
+          ((f x) x)
+          (else (loop (+ x 1))))))
+
+(define-syntax (Min stx)
+  (syntax-parse stx
+    #:literals (≦)
+    [(_ v:id ≦ max:expr body:expr)
+     #'(Min≦ max (λ (v) body))]))
+```
+
+`min`だと名前がカブるので`Min`にしました
+
+`Min≦`や`Min`は`(f x)`を満たす`x`が見つからなかった場合は0を返します
+`x`=0から開始すると見つからなかったのか`x`=0で見つかったのかわからないので
+`x`=1から開始することにしました
+`∀`や`∃`も同様に修正
--- incompleteness.rkt
+++ incompleteness.rkt
@@ -6,7 +6,7 @@
 ; 装備を整える
 
 (define (∀≦ max f)
-  (let loop ((x 0))
+  (let loop ((x 1))
     (cond ((> x max) #t)
           ((not (f x)) #f)
           (else (loop (+ x 1))))))
@@ -16,6 +16,7 @@
 
 ; literalは何か定義しないとエラーになる
 ; とりあえず定義されていれば値はなんでもいい模様
+
 (define ≦ #f)
 
 (define-syntax (∀ stx)
@@ -28,7 +29,7 @@
 (check-false (∀ x ≦ 3 (< x 3)))
 
 (define (∃≦ max f)
-  (let loop ((x 0))
+  (let loop ((x 1))
     (cond ((> x max) #f)
           ((f x) #t)
           (else (loop (+ x 1))))))
@@ -45,5 +46,26 @@
 (check-true (∃ x ≦ 3 (= x 2)))
 (check-false (∃ x ≦ 3 (= x 4)))
 
+; (f x)を満たすxが見つからなかった場合は0を返す
+; x=0から開始すると見つからなかったのかx=0で見つかったのかわからないので
+; x=1から開始する
+
+(define (Min≦ max f)
+  (let loop ((x 1))
+    (cond ((> x max) 0)
+          ((f x) x)
+          (else (loop (+ x 1))))))
+
+(check-eq? (Min≦ 3 (λ (x) (= x 2))) 2)
+(check-eq? (Min≦ 3 (λ (x) (= x 4))) 0)
+
+(define-syntax (Min stx)
+  (syntax-parse stx
+    #:literals (≦)
+    [(_ v:id ≦ max:expr body:expr)
+     #'(Min≦ max (λ (v) body))]))
+
+(check-eq? (Min x ≦ 3 (= x 2)) 2)
+(check-eq? (Min x ≦ 3 (= x 4)) 0)
 
 
